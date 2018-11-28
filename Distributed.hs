a02ce32b1cbf62ce41ca40e955a77f58e33d1380
Ruud Koot <inbox@ruudkoot.nl>
2018-11-28 21:02:24 +0000

    add process labels and Time

--- Distributed.hs
+++ Distributed.hs
@@ -6,19 +6,25 @@
 module Distributed where
 
 import Control.Monad
+import Control.Monad.Trans
 import Control.Monad.Trans.Free
+import Control.Monad.Trans.State
 
 -- | Process monad
+
+type Clk = Int
 
 data ProcessF msg m next
     = forall a. Tau (m a) (a -> next)
     | Send msg next
     | Recv (msg -> next)
+    | Time (Clk -> next)
 
 instance Functor (ProcessF msg m) where
     fmap f (Tau action next) = Tau action (f . next)
     fmap f (Send msg next) = Send msg (f next)
     fmap f (Recv next) = Recv (f . next)
+    fmap f (Time next) = Time (f . next)
 
 type ProcessT msg m a = FreeT (ProcessF msg m) m a
 
@@ -31,24 +37,32 @@ send msg = liftF (Send msg ())
 recv :: (Monad m) => ProcessT msg m msg
 recv = liftF (Recv id)
 
+time :: (Monad m) => ProcessT msg m Clk
+time = liftF (Time id)
+
 -- * Schedulers
 
-roundRobin :: (Monad m) => [ProcessT msg m a] -> m ()
+roundRobin :: (Monad m) => [(String, ProcessT msg m a)] -> m ()
 roundRobin = roundRobin' []
 
-roundRobin' :: (Monad m) => [msg] -> [ProcessT msg m a] -> m ()
-roundRobin' ms [] = return ()
-roundRobin' ms (p:ps) = runFreeT p >>= \case
+roundRobin' :: (Monad m) => [msg] -> [(String, ProcessT msg m a)] -> m ()
+roundRobin' mq [] = return ()
+roundRobin' mq ((lbl, p) : pq) = runFreeT p >>= \case
     Free (Tau action next) -> do
         x <- action
-        roundRobin' ms (ps ++ [next x])
-    Free (Send msg next) ->
-        roundRobin' (ms ++ [msg]) (ps ++ [next])
-    Free (Recv next) -> case ms of
-        [] -> roundRobin' ms (ps ++ [p])
-        (m:ms) -> roundRobin' ms (ps ++ [next m])
-    Pure x ->
-        roundRobin' ms ps
+        roundRobin' mq (pq ++ [(lbl, next x)])
+    Free (Send msg next) -> do
+        roundRobin' (mq ++ [msg]) (pq ++ [(lbl, next)])
+    Free (Recv next) -> do
+        case mq of
+            [] -> do
+                roundRobin' mq (pq ++ [(lbl, p)])
+            (msg : mq) -> do
+                roundRobin' mq (pq ++ [(lbl, next msg)])
+    Free (Time next) -> do
+        roundRobin' mq (pq ++ [(lbl, next 0)])
+    Pure x -> do
+        roundRobin' mq pq
 
 -- | Example processes
 
